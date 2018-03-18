638f9bc8078484bcd8c6924ae759c6155e2ef750
Bethany Drake <bethanyd@thoughtworks.com>
2018-03-19 10:42:47 +1100

    booklist should include book details

--- src/com/twu/biblioteca/BooklistWindow.java
+++ src/com/twu/biblioteca/BooklistWindow.java
@@ -1,12 +1,18 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BooklistWindow extends Window {
 
+
+
+    public ArrayList<Book> books;
 
     public BooklistWindow() {
         super();
         setTitle("Booklist");
         setText("Harry Potter\nTwilight\n");
+
 
     }
 
@@ -16,4 +22,21 @@ public class BooklistWindow extends Window {
 
     }
     */
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        text = "";
+        for (Book b : books)
+        {
+            text += b.title+", " + b.author + ", " + b.year + "\n";
+
+        }
+
+
+        this.books = books;
+    }
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,6 +2,9 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -23,19 +26,7 @@ public class ExampleTest {
     }
 
 
-    @Test
-    public void shouldDisplayListOfBooksWhenBooklistWindowIsOpen()
-    {
-        IOManager m = new IOManager();
-        BooklistWindow window = new BooklistWindow();
-        m.currentWindow = window;
 
-        String LIST_OF_BOOKS = "Harry Potter\nTwilight\n";
-        //window.open();
-        String output = m.getOutput();
-        assertEquals(LIST_OF_BOOKS,output);
-
-    }
 
     @Test
     public void shouldProceedToBooklistAfterWelcomeScreenAutomatically()
@@ -49,6 +40,22 @@ public class ExampleTest {
 
         assertEquals(true, isBooklistCurrentWindow);
         assertEquals(1, m.numWaitingOutputs);
+
+    }
+
+    @Test
+    public void bookListShouldIncludeBookDetails()
+    {
+       BooklistWindow booklistWindow = new BooklistWindow();
+       ArrayList<Book> booklist  = new ArrayList<Book>();
+       booklist.add(new Book("title1", "author1",2001));
+       booklist.add(new Book("title2", "author2", 2002));
+
+       booklistWindow.setBooks(booklist);
+
+
+       String expected = "title1, author1, 2001\ntitle2, author2, 2002\n";
+       assertEquals(expected, booklistWindow.getText());
 
     }
 
